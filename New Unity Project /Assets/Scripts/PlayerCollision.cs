607e8eb3750d5b2010fd16e4ab067858ba6c6729
Monira Khan <monirakhan21@gmail.com>
2018-06-21 09:52:48 -0400

    Basic game code

---
+++ New Unity Project /Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+
+    void OnCollisionEnter(Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Obsticale") {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+	}
+}
